3f4fd3c4a1e1d00e55fafdbc561e5d67c9073a74
Carlos Felipe Agudelo <cf.agudelo12@uniandes.edu.co>
2016-03-29 18:40:11 -0500

    Se cambia a ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,29 +1,36 @@
-angular.module('treeApp', ['ui.tree','ngRoute']).config(['$routeProvider', function($routeProvider) { 
-    $routeProvider
-        .when('/', {
-            templateUrl : 'views/signup-login.html',
-            controller: 'Signup-LoginCtrl'
-        })
-        .when('/main', {
-            templateUrl : 'views/main.html',
-            controller: 'MainCtrl'
-        })
-        .when('/versions', {
-            templateUrl : 'views/versions.html',
-            controller: 'VersionsCtrl'
-        })
-        .when('/editing', {
-            templateUrl : 'views/editing.html',
-            controller: 'EditorCtrl'
-        })
-        .when('/form', {
-            templateUrl : 'views/form.html',
-            controller: 'FormCtrl'
-        })
-        .when('/featureModelSettings', {
-            templateUrl : 'views/featureModelSettings.html',
-            controller: 'featureModelSettingsCtrl'
-        });
-  }]);
+angular.module('treeApp', ['ui.tree','ui.router']).config(function($stateProvider, $urlRouterProvider) { 
+    $urlRouterProvider.otherwise("/");
+    $stateProvider
+    .state('home', {
+      url: "/home",
+      templateUrl: "views/home.html",
+      controller: 'MainCtrl'
+    })
+    .state('creating', {
+      url: "/create",
+      templateUrl: "views/creating.html",
+      controller: 'FormCtrl'
+    })
+    .state('editing', {
+      url: "/editor",
+      templateUrl: "views/editing.html",
+      controller: 'EditorCtrl'
+    })
+    .state('setting', {
+      url: "/settigs",
+      templateUrl: "views/settings.html",
+      controller: 'featureModelSettingsCtrl'
+    })
+    .state('versioning', {
+      url: "/versions",
+      templateUrl: "views/versions.html",
+      controller: 'VersionsCtrl'
+    })
+    .state('signup-login', {
+      url: "/",
+      templateUrl: "views/signup-login.html",
+      controller: 'Signup-LoginCtrl'
+    });
+  });
 
 
