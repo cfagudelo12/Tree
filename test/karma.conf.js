972b841599508b8b47844c21cd9f8492fca6f729
Carlos Felipe Agudelo <cf.agudelo12@uniandes.edu.co>
2016-04-07 21:02:28 -0500

    Se agrega angular ui-tree

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -27,6 +27,7 @@ module.exports = function(config) {
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/firebase/firebase.js',
       'bower_components/angularfire/dist/angularfire.js',
+      'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
